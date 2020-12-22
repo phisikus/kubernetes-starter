6cfa6bbdf6f1b48e988415b1a52860af75df86f7
Marcin Biernacki <phisikus@platinum.linux.pl>
2020-12-22 12:09:30 +0100

    Fix flannel configuration

--- install-networking.sh
+++ install-networking.sh
@@ -2,5 +2,9 @@
 
 # Install Flannel networking layer
 export KUBECONFIG=/etc/kubernetes/admin.conf
-kubectl create -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+## Change used interface to avoid routing errors due to default usage of first interface (which uses NAT)
+cat kube-flannel.yml | sed 's/--kube-subnet-mgr/--kube-subnet-mgr\n        - --iface=enp0s8/' > flannel.yml
+rm kube-flannel.yml
+kubectl apply -f flannel.yml
 kubectl taint nodes --all node-role.kubernetes.io/master-
